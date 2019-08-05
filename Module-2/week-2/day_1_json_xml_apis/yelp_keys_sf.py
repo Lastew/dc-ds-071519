74708bf387c0e909967d9562252fafeb89b05bd8
Sisay <sisfilate@gmail.com>
2019-08-05 14:51:59 -0400

    class example inculded

--- Module-2/week-2/day_1_json_xml_apis/yelp_keys_sf.py
+++ Module-2/week-2/day_1_json_xml_apis/yelp_keys_sf.py
@@ -2,10 +2,10 @@
 
 def return_api_key():
     """returns api key for yelp api"""
-    api_key = ''
+    api_key = 'ZyzQubnIhCTFsyRxgRHd1zSlt01J-o7LhiwGaYtY12cZiae2NU2cU1wy-YzypC57rWwouZavP2EsQOcj8JYIPWJZx2g4lQc8gy16IUY1w35eY8jBrTV1kN2MwmlIXXYx'
     return api_key
 
 def return_client_id():
     """Returns client id for yelp api"""
-    client_id = ''
+    client_id = 'GQeXr5kbcbpXuC7ZJzB1VQ'
     return client_id
