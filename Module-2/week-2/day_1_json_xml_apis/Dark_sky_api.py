2830a8f45f159641a10ad40c53f687256e63a972
Sisay <sisfilate@gmail.com>
2019-08-13 13:05:33 -0400

    works

---
+++ Module-2/week-2/day_1_json_xml_apis/Dark_sky_api.py
@@ -0,0 +1,6 @@
+"""stores the Dark Sky api api key in a separate file for security and best practice"""
+
+def return_api_key():
+    """returns api key for Dark Sky api"""
+    api_key = '8622f665bf1103b6b6e49922fa68d965'
+    return api_key
